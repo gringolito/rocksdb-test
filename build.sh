4449e61f1cdc6ae0bb15f52afe5165a562de333c
Filipe Utzig <filipeutzig@gmail.com>
2019-05-26 22:30:31 -0300

    Update project to Modern CMake
    Use modern CMake pratices available in CMake 3.13 to propagate compile
    features, include directories and linking libraries across the targets.
    
    Signed-off-by: Filipe Utzig <filipeutzig@gmail.com>

--- build.sh
+++ build.sh
@@ -2,17 +2,19 @@
 bin_name=$(basename "${0}")
 
 function print_help() {
-    echo -e "Usage: ${bin_name} [-v] [-h]"
+    echo -e "Usage: ${bin_name} [-v] [-h] [-l]"
     echo -e ""
     echo -e "Options:"
     echo -e "  -v        Turn make output verbose"
     echo -e "  -h        Print this help message"
+    echo -e "  -l        Use Linux specific stubs"
     exit "${1}"
 }
 
-while getopts vh OPT; do
+while getopts vhl OPT; do
     case "${OPT}" in
         v) verbose="-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON" ;;
+        l) stubs='-DMPSYNC_STUBS="linux"' ;;
         h) print_help 0 ;;
         *) print_help 1 ;;
     esac
@@ -30,12 +32,19 @@ test -n "${rocksdb_repo_installed}" || conan remote add koeleck https://api.bint
 fswatch_repo_installed=$(conan remote list | grep 'https://api.bintray.com/conan/bincrafters/public-conan')
 test -n "${fswatch_repo_installed}" || conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 
-set -e
-conan install -s compiler.libcxx=libstdc++11 --build missing ..
+os=$(uname -s)
+case "${os}" in
+    Darwin) conan_options="-s compiler.libcxx=libc++" ;;
+    Linux) conan_options="-s compiler.libcxx=libstdc++11" ;;
+    *) ;;
+esac
 
+set -e
+conan install ${conan_options} --build missing ..
+rm -f FindRocksDB.cmake # Remove unwantd Conan generated FindPackage
 ####################################################################################################
 # Build the project
 ####################################################################################################
-cmake -DCMAKE_TOOLCHAIN_FILE=conan_paths.cmake -DMPSYNC_BUILD_STUBS=yes -DBUILD_TESTING=yes ${verbose} ..
+cmake -DCMAKE_TOOLCHAIN_FILE=conan_paths.cmake -DMPSYNC_BUILD_STUBS=yes ${stubs} -DBUILD_TESTING=yes ${verbose} ..
 cmake --build .
 cp compile_commands.json ..
